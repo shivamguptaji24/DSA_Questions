baff2e2782f8d164df0995fe56b2aca47b4cc284
Shivam Gupta <shivamguptaji24@gmail.com>
2025-03-15 23:41:52 +0530

    Solution of Stock Buy And Sell

--- Day 1: Arrays/StockBuyAndSell.java
+++ Day 1: Arrays/StockBuyAndSell.java
@@ -1,40 +1,8 @@
 /*
 It can be solve by two approach :-
-    1. Optimal Approach
-    2. Brute Force Approach
+    1. Brute Force Approach
+    2. Optimal Approach
 */
-
-// Optimal Approach
-
-/*
-Intuition: We will linearly travel the array. We can maintain a minimum from the start of the array and compare it with every element of the array, if it is greater than the minimum then take the difference and maintain it in max, otherwise update the minimum.
-
-Approach:
-
-1. Create a variable maxPro and store 0 initially.
-2. Create a variable minPrice and store some larger value(ex: MAX_VALUE) value initially.
-3. Run a for loop from 0 to n.
-4. Update the minPrice if it is greater than the current element of the array
-5. Take the difference of the minPrice with the current element of the array and compare and maintain it in maxPro.
-6. Return the maxPro.
-
-Time complexity: O(n)
-Space Complexity: O(1)
-*/
-
-class Solution {
-    public int maxProfit(int[] prices) {
-        int maxPro = 0;
-        int minPrice = Integer.MAX_VALUE;
-        for (int i = 0; i < prices.length; i++) {
-            minPrice = Math.min(minPrice, prices[i]);
-            maxPro = Math.max(maxPro, prices[i] - minPrice);
-        }
-        return maxPro;
-    }
-}
-
-/*-------------------------------------------------------------------------------------------------------------------------------------------*/
 
 // Brute Force Approach
 
@@ -62,6 +30,38 @@ class Solution {
                     maxPro = Math.max(prices[j] - prices[i], maxPro);
                 }
             }
+        }
+        return maxPro;
+    }
+}
+
+/*-------------------------------------------------------------------------------------------------------------------------------------------*/
+
+// Optimal Approach
+
+/*
+Intuition: We will linearly travel the array. We can maintain a minimum from the start of the array and compare it with every element of the array, if it is greater than the minimum then take the difference and maintain it in max, otherwise update the minimum.
+
+Approach:
+
+1. Create a variable maxPro and store 0 initially.
+2. Create a variable minPrice and store some larger value(ex: MAX_VALUE) value initially.
+3. Run a for loop from 0 to n.
+4. Update the minPrice if it is greater than the current element of the array
+5. Take the difference of the minPrice with the current element of the array and compare and maintain it in maxPro.
+6. Return the maxPro.
+
+Time complexity: O(n)
+Space Complexity: O(1)
+*/
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxPro = 0;
+        int minPrice = Integer.MAX_VALUE;
+        for (int i = 0; i < prices.length; i++) {
+            minPrice = Math.min(minPrice, prices[i]);
+            maxPro = Math.max(maxPro, prices[i] - minPrice);
         }
         return maxPro;
     }
