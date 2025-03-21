ba7f685332d0d3df3a84aa1e369254d7eafa4a67
Shivam Gupta <shivamguptaji24@gmail.com>
2025-03-21 23:48:58 +0530

    Solution of Pascal's Triangle

--- Day 1: Arrays/Pascal's_Triangle.java
+++ Day 1: Arrays/Pascal's_Triangle.java
@@ -49,6 +49,98 @@ class Solution {
 
 /*-------------------------------------------------------------------------------------------------------------------------*/
 
+// Variation 2
+
+/*
+It can be solve by two approach :-
+  1. Naive Approach
+  2. Optimal Approach
+*/
+
+// Naive Approach
+
+/*
+In this approach, for every column from 1 to n, we will calculate the element (n, c)(where n is the given row number and c is the column number that will vary from 1 to n) using the previous method. Thus, we will print the row.  
+
+Approach:
+The steps are as follows:
+
+1. We will use a loop(say c) to iterate over each column i.e. from 1 to n. And for each column, we will do the following steps:
+      a. First, we will consider n-1 as n and c-1 as r.
+      b. After that, we will simply calculate the value of the combination using a loop. 
+      c. The loop will run from 0 to r. And in each iteration, we will multiply (n-i) with the result and divide the result by (i+1).
+      d. Finally, we will print the element.
+2. Finally, the entire row will be printed.
+
+Time Complexity: O(n*r), where n is the given row number, and r is the column index which can vary from 0 to n-1.
+Reason: We are calculating the element for each column. Now, there are total n columns, and for each column, the calculation of the element takes O(r) time where r is the column index.
+Space Complexity: O(1) as we are not using any extra space.
+*/
+
+class Solution {
+    public long nCr(int n, int r) {
+        long res = 1;
+
+        // calculating nCr:
+        for (int i = 0; i < r; i++) {
+            res = res * (n - i);
+            res = res / (i + 1);
+        }
+        return res;
+    }
+
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> row = new ArrayList<>();
+
+        // Generating the entire row `rowIndex`:
+        for (int col = 0; col <= rowIndex; col++) {
+            row.add((int) nCr(rowIndex, col));
+        }
+        return row;
+    }
+}
+
+// Optimal Approach
+
+/*
+We will try to build intuition for this approach using the following observations.
+
+Here, we can observe that the numbers marked in red are added to the previous number to build the current number. In each step, the numerator is multiplied by the previous consecutive element, and the denominator is multiplied by the next consecutive element.
+We will replicate this in our algorithm. First, we will mark the indices of the column starting from 0. Then we will simply multiply the numerator by (n-index) and the denominator by the index itself.
+From column no. 1 the formula will be the following:
+Current element = prevElement * (rowNumber - colIndex) / colIndex
+
+Approach:
+The steps are as follows:
+
+1. First, we will print the 1st element i.e. 1 manually.
+2. After that, we will use a loop(say i) that runs from 1 to n-1. It will print the rest of the elements.
+3. Inside the loop, we will use the above-said formula to print the element. We will multiply the previous answer by (n-i) and then divide it by i itself.
+4. Thus, the entire row will be printed.
+Note: If we want to store the row elements, we just need to store each element instead of printing it.
+
+Time Complexity: O(N) where N = given row number. Here we are using only a single loop.
+Space Complexity: O(1) as we not using any extra space.
+*/
+
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> row = new ArrayList<>();
+        long ans = 1;
+        row.add((int) ans); // Adding the first element (always 1)
+
+        // Generate the rest of the row:
+        for (int i = 1; i <= rowIndex; i++) {
+            ans = ans * (rowIndex - i + 1);
+            ans = ans / i;
+            row.add((int) ans);
+        }
+        return row;
+    }
+}
+
+/*-------------------------------------------------------------------------------------------------------------------------*/
+
 // Variation 3
 
 /*
@@ -124,3 +216,30 @@ Reason: We are generating a row for each single row. The number of rows is n. An
 Space Complexity: In this case, we are only using space to store the answer. That is why space complexity can still be considered as O(1).
 */
 
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> ans = new ArrayList<>();
+
+        // Generate Pascal's Triangle row by row
+        for(int row = 1; row <= numRows; row++) {
+            ans.add(generateRow(row));
+        }
+
+        return ans;
+    }
+
+    public List<Integer> generateRow(int row) {
+        long ans = 1;
+        List<Integer> ansRow = new ArrayList<>();
+        ansRow.add(1);  // First element is always 1
+
+        // Calculating the remaining elements using nCr formula
+        for(int col = 1; col < row; col++) {
+            ans = ans * (row - col);
+            ans = ans / col;
+            ansRow.add((int) ans);
+        }
+
+        return ansRow;
+    }
+}
